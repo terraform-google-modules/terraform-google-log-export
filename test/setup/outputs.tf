049fa40cd9291dfc804c87a95e503e066e4aed02
Morgante Pell <morgantep@google.com>
2019-12-12 16:25:53 -0500

    Fix sa_key output

--- test/setup/outputs.tf
+++ test/setup/outputs.tf
@@ -18,7 +18,7 @@ output "project_id" {
   value = module.project.project_id
 }
 
-output "sa_json" {
+output "sa_key" {
   value     = google_service_account_key.int_test.private_key
   sensitive = true
 }
