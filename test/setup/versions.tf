0f4a8320a55134fa52f8a0b23c9a4bc1055a7ee4
Brandon J. Bjelland <brandoconnor@gmail.com>
2020-08-28 12:29:10 -0700

    fix: relax version constraints to enable terraform 0.13.x compatibility (#57)
    * fix: relax version constraints to enable terraform 0.13.x compatibility
    
    * fix: updating project-factory to help CI
    
    * fix: attempting to fix CI with newer provider versions as versioning seems to be the root
    
    * fix: shortening CI cycle w PF skipping gcloud download

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -15,13 +15,13 @@
  */
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">=0.12.6, <0.14"
 }
 
 provider "google" {
-  version = "~> 3.5.0"
+  version = "~> 3.36.0"
 }
 
 provider "google-beta" {
-  version = "~> 3.5.0"
+  version = "~> 3.36.0"
 }
