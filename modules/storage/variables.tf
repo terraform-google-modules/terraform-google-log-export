eb0d4ef047c5354644a7ba01f66dc66e9e5cfd2d
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2020-10-13 14:03:15 -0300

    Fixes formating

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -56,7 +56,7 @@ variable "expiration_days" {
 variable "retention_policy" {
   description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
   type = object({
-    is_locked        = bool
+    is_locked             = bool
     retention_period_days = number
   })
   default = null
