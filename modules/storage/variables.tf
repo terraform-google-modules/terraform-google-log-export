a6ac14f9101dc48e4a7d84b40aee3b9e5fdb3bdb
Amanda Karina Lopes de Oliveira <amandak@ciandt.com>
2020-10-13 12:06:33 -0300

    Adds retention policy support

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -52,3 +52,12 @@ variable "expiration_days" {
   type        = number
   default     = null
 }
+
+variable "retention_policy" {
+  description = "Configuration of the bucket's data retention policy for how long objects in the bucket should be retained."
+  type = object({
+    is_locked        = bool
+    retention_period = number
+  })
+  default = null
+}
