22b94a65cbcd87940789c7296f6bc1e5978a4c33
Amanda Karina Lopes de Oliveira <55760933+amandakarina@users.noreply.github.com>
2020-10-15 13:01:01 -0300

    feat: Add retention policy support (#68)
    * Adds retention policy support
    
    * Adds blank line at the end of file
    
    * Adds units on variable name
    
    * Changes retention variable to day format
    
    Co-authored-by: Umair Idris <umairidris@google.com>
    
    * Updates README
    
    * Fixes formating
    
    * Fixes variables name
    
    Co-authored-by: Umair Idris <umairidris@google.com>

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -52,6 +52,14 @@ resource "google_storage_bucket" "bucket" {
       }
     }
   }
+
+  dynamic "retention_policy" {
+    for_each = var.retention_policy == null ? [] : [var.retention_policy]
+    content {
+      is_locked        = var.retention_policy.is_locked
+      retention_period = var.retention_policy.retention_period_days * 24 * 60 * 60 // days to seconds
+    }
+  }
 }
 
 #--------------------------------#
