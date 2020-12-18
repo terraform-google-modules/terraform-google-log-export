d6361e0c4d035f04a20c4b168fb6c310349663e1
Daniel da Silva Andrade <dandrade@ciandt.com>
2020-12-18 00:52:19 -0300

    Rename superadmin use case

--- modules/bq-log-alerting/use-cases/superadmin_login.sql
+++ modules/bq-log-alerting/use-cases/superadmin_login.sql
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This a 'v2' use case because it uses a different log source than the original
-
 SELECT
   timestamp AS eventTimestamp,
   receiveTimestamp,
