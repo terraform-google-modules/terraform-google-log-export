1636eed7d3ac6cb54d79d7849034a11613399dca
Sumeet Chaurasia <sumeet.chaurasia@quantiphi.com>
2021-07-02 23:20:08 +0530

    feat!: Support lifecycle rules and labels for GCS submodule (#96)
    * added labels for gcs destination
    
    * formatted
    
    * 1. Added lifecycle rules instead of just expiration days in GCS destination, updated relevant examples. 2. Added opinion based service account creation for subscriber, not always a service account would be required
    
    * added new line
    
    * added upgrade document
    
    * reverted pubsub subscription service account flag modifications

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -38,20 +38,26 @@ resource "google_storage_bucket" "bucket" {
   location                    = var.location
   force_destroy               = var.force_destroy
   uniform_bucket_level_access = var.uniform_bucket_level_access
+  labels                      = var.storage_bucket_labels
 
   versioning {
     enabled = var.versioning
   }
 
   dynamic "lifecycle_rule" {
-    for_each = var.expiration_days == null ? [] : [var.expiration_days]
+    for_each = var.lifecycle_rules
     content {
       action {
-        type = "Delete"
+        type          = lifecycle_rule.value.action.type
+        storage_class = lookup(lifecycle_rule.value.action, "storage_class", null)
       }
       condition {
-        age        = var.expiration_days
-        with_state = "ANY"
+        age                    = lookup(lifecycle_rule.value.condition, "age", null)
+        created_before         = lookup(lifecycle_rule.value.condition, "created_before", null)
+        with_state             = lookup(lifecycle_rule.value.condition, "with_state", lookup(lifecycle_rule.value.condition, "is_live", false) ? "LIVE" : null)
+        matches_storage_class  = contains(keys(lifecycle_rule.value.condition), "matches_storage_class") ? split(",", lifecycle_rule.value.condition["matches_storage_class"]) : null
+        num_newer_versions     = lookup(lifecycle_rule.value.condition, "num_newer_versions", null)
+        days_since_custom_time = lookup(lifecycle_rule.value.condition, "days_since_custom_time", null)
       }
     }
   }
