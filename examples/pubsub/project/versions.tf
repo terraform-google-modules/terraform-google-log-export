ddc4f89c33e7b73a67686d1fd284f403ad1b4b8c
Donatas Navidonskis <donnav@boozt.com>
2022-03-24 21:28:01 +0200

    upgrade provider version

--- examples/pubsub/project/versions.tf
+++ examples/pubsub/project/versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 4.14.0"
     }
     random = {
       source = "hashicorp/random"
