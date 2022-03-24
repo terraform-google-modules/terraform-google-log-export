20464abac8d8eebdd4ebaa4a79649b01171a7022
Donatas Navidonskis <donnav@boozt.com>
2022-03-24 09:51:16 +0200

    update subscription tests

--- test/integration/pubsub/project/controls/gcp.rb
+++ test/integration/pubsub/project/controls/gcp.rb
@@ -35,11 +35,10 @@ control "gcp" do
     its('writer_identity') { should eq log_export_map[:writer_identity] }
   end
 
-  describe google_pubsub_subscription(
-    project: destination_map[:project],
-    name: "#{destination_map[:resource_name]}-subscription"
-  ) do
-    it { should exist }
-    its('message_retention_duration') { should eq "600s" }
+  describe google_pubsub_subscriptions(project: destination_map[:project]).names.each do |subscription_name|
+    describe google_pubsub_subscription(project: destination_map[:project], name: subscription_name) do
+      it { should exist }
+      its('message_retention_duration') { should eq "600s" }
+    end
   end
 end
