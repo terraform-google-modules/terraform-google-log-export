9b9c72ba9ec687c4bb928c7da371fd78d5b5c1d7
g-awmalik <malik.awais@gmail.com>
2022-05-31 16:30:34 -0700

    Update modules/logbucket/versions.tf
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- modules/logbucket/versions.tf
+++ modules/logbucket/versions.tf
@@ -25,7 +25,7 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-log-export:storage/v7.3.0"
+    module_name = "blueprints/terraform/terraform-google-log-export:logbucket/v7.3.0"
   }
 
 }
