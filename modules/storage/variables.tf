2bc9fa6ec3ee9b8f78ecea4f7b888b5e461d206c
salonipatidar1999@gmail.com <salonipatidar1999@gmail.com>
2023-11-01 02:57:44 +0530

    formatting terraform

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -108,6 +108,6 @@ variable "data_locations" {
 
 variable "public_access_prevention" {
   description = "Prevents public access to a bucket. Acceptable values are \"inherited\" or \"enforced\". If \"inherited\", the bucket uses public access prevention. only if the bucket is subject to the public access prevention organization policy constraint."
-  type = string
-  default = "inherited"
+  type        = string
+  default     = "inherited"
 }
