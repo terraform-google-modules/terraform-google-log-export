9b40194275135c0e29c4e681db883b22315c390f
Benedikt Böhm <bb@xnull.de>
2023-11-06 16:14:53 +0100

    fix: update version constraints for TPG v5

--- examples/datadog-sink/versions.tf
+++ examples/datadog-sink/versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = ">= 3.53, < 6"
     }
     local = {
       source = "hashicorp/local"
